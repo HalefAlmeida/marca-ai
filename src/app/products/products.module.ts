1589955892fcd1e740cc32384a5ca9aef04c7a78
halefalmeida <halef.dorigan@outlook.com>
2021-06-13 12:26:06 -0300

    feat: products card

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,14 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsPageComponent } from './products-page/products-page.component';
 
-
-
 @NgModule({
-  declarations: [
-    ProductsPageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ProductsPageComponent],
+  imports: [CommonModule],
 })
-export class ProductsModule { }
+export class ProductsModule {}
